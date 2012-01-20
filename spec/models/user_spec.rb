1ba32ceee248687997fb0ee2bc7c786339fd011b
Sam Zhai <sam@tbocs.org>
2012-01-20 11:24:41 -0800

    email validations lol

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,5 +12,49 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @attr = { :name => "Example User", :email => "user@example.com" }
+  end
+
+  it "should create a new instance given valid attributes" do
+    User.create!(@attr)
+  end
+
+  it "should require a name" do
+    user_no_name = User.new(@attr.merge(:name => ""))
+    user_no_name.should_not be_valid
+  end
+
+  it "should require an email address" do
+    user_no_email = User.new(@attr.merge(:email => ""))
+    user_no_email.should_not be_valid
+  end
+
+  it "should reject a name that is too long" do
+    user_long_name = User.new(@attr.merge(:name => "a" * 51))
+    user_long_name.should_not be_valid
+  end
+
+  it "should accept valid email addresses" do
+    addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
+    addresses.each do |address|
+      valid_email_user = User.new(@attr.merge(:email => address))
+      valid_email_user.should be_valid
+    end
+  end
+
+  it "should reject invalid email addresses" do
+    addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
+    addresses.each do |address|
+      invalid_email_user = User.new(@attr.merge(:email => address))
+      invalid_email_user.should_not be_valid
+    end
+  end
+
+  it "should reject duplicate email addresses" do
+    User.create!(@attr)
+    user_with_duplicate_email = User.new(@attr.merge(:email => @attr[:email].upcase))
+    user_with_duplicate_email.should_not be_valid
+  end 
+
 end
