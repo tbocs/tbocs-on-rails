48d3e1aafeb574c8ca35c4e12cd13b2c110c8001
Sam Zhai <sam@tbocs.org>
2012-01-21 18:18:05 -0800

    sign in out done

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,11 @@ SampleApp::Application.routes.draw do
   match '/signup', :to => 'users#new'
  
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
+  
+  match '/signup', :to => 'users#new'
+  match '/signin', :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
